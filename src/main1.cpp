794dd41649d17502c4ed93e8c6853bb6cb957fd4
Alison <sidneialison@gmail.com>
2018-05-23 20:00:26 -0300

    Tratamento de acentos adicionado

--- src/main1.cpp
+++ src/main1.cpp
@@ -5,25 +5,29 @@
 #include "pilha.h"
 
 
-// std::string RemoverAcentos(std::string in){
-// 	std::vector<string> comAcentos = {"Á", "Â", "À", "Ã", "á", "â", "à", "ã", "É", "Ê", "È", "é", "ê", "è", "Í", "Î", "Ì", "í",
-// 	 "î", "ì", "Ó", "Ô", "Ò", "Õ", "ó", "ô", "ò", "õ", "Ú", "Û", "ú", "û", "ù", "Ç", "ç"};
-// 	std::vector<string> semAcentos = {"A", "A", "A", "A", "a", "a", "a", "a", "E", "E", "E", "e", "e", "e", "I", "I", "I", "i",
-// 	 "i", "i", "O", "O", "O", "O", "o", "o", "o", "o", "U", "U", "u", "u", "u", "C", "c"};
+std::string RemoverAcentos(std::string in){
+	std::vector<string> comAcentos = {"Á", "Â", "À", "Ã", "á", "â", "à", "ã", "É", "Ê", "È", "é", "ê", "è", "Í", "Î", "Ì", "í",
+	 "î", "ì", "Ó", "Ô", "Ò", "Õ", "ó", "ô", "ò", "õ", "Ú", "Û", "ú", "û", "ù", "Ç", "ç"};
+	std::vector<string> semAcentos = {"A", "A", "A", "A", "a", "a", "a", "a", "E", "E", "E", "e", "e", "e", "I", "I", "I", "i",
+	 "i", "i", "O", "O", "O", "O", "o", "o", "o", "o", "U", "U", "u", "u", "u", "C", "c"};
 	
-// 	for (unsigned i = 0; i < comAcentos.size(); i++){
-// 		std::cout << comAcentos[i] << std::endl;
-// 		in.replace(in.begin(), in.end(), comAcentos[i], semAcentos[i]);
-// 	}
+	for (unsigned i = 0; i < in.size(); i++){
+		for (unsigned j = 0; j < comAcentos.size(); j++){
+			std::size_t found = in.find(comAcentos[j]);
+			if ( found != std::string::npos ){
+				in.replace( found, comAcentos[j].size(), semAcentos[j] );
+			}
+		}
+	}
 
-// 	return in;
-// }
+	return in;
+}
 
 std::string TratarString(std::string in){
 
 	std::string aux="";
 
-	//in = RemoverAcentos(in);
+	in = RemoverAcentos(in);
 
 	for (unsigned i = 0; i < in.size(); i++){
 		if((in[i] >= 65 && in[i] <= 90) || (in[i] >= 97 && in[i] <= 122)) {
@@ -57,8 +61,8 @@ int main (int argc, char const* argv[]){
 	    s.pop();
 	}
 
-	// std::cout << in << std::endl;
-	// std::cout << out << std::endl;
+	std::cout << in << std::endl;
+	std::cout << out << std::endl;
 
 	in == out ? std::cout << "É palindromo!" << std::endl : std::cout << "Não é palindromo!" << std::endl;
 
